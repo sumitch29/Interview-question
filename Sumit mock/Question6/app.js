0b15dcc089ea2221c35e1ba20b7a7ab03c399863
Sumit Chavhan <srchavhan29@gmail.com>
2021-11-10 10:36:53 +0530

    question6

---
+++ Sumit mock/Question6/app.js
@@ -0,0 +1,8 @@
+const inPut = document.querySelector("#loading");
+const loadbtn = document.querySelector("#loadBtn");
+
+loadbtn.addEventListener('click',hide)
+
+function hide(){
+    inPut.style.display = "none"
+}
