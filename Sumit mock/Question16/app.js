0d7db7fefdabea3509ca65e363f2a4064c85fe48
Sumit Chavhan <srchavhan29@gmail.com>
2021-11-12 23:38:10 +0530

    question15

---
+++ Sumit mock/Question16/app.js
@@ -0,0 +1,17 @@
+const findBtn = document.querySelector("#find");
+const outPut = document.querySelector("#output");
+
+let ramData = {
+    name: "ram",
+    age: "27",
+    power: "2500",
+    yuga: "treta"
+}
+
+let krishnaData ={
+    name: "krishna",
+    age: "30",
+    power: "2300",
+    yuga: "Dwaper"
+}
+
