717ff23515efcff033c20b8c84243e4efaae00cf
Sumit Chavhan <srchavhan29@gmail.com>
2021-11-28 12:52:30 +0530

    finded diffrent ways

--- Sumit mock/Question8/app.js
+++ Sumit mock/Question8/app.js
@@ -5,8 +5,35 @@ const URL = "https://unitube-server.herokuapp.com/playlists"
 
 callBtn.addEventListener('click',clickHandler)
 
+// function clickHandler () {
+//     fetch(URL) 
+//     .then(response => response.json())
+//     .then(data => outPut.innerText = `${data.message}`)
+// }
+
+// function clickHandler(){
+//     fetch(URL)
+//     .then(response =>{
+//         if(response.status === 404){
+//             outPut.innerText = response.status
+//         }
+//     })
+// }
+
+// function clickHandler () {
+//     fetch(URL) 
+//     .then(response => {
+//         if (response.status === 401 ) {
+//              outPut.innerText=  response.statusText  
+ 
+//               console.log(response);     
+//         }
+//     }
+//     )
+// }
+
+
 function clickHandler () {
     fetch(URL) 
-    .then(response => response.json())
-    .then(data => outPut.innerText = `${data.message}`)
+    .then(response =>  outPut.innerText = `${response.status}  ${response.message}`)
 }
