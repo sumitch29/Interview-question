59cba837d3ba1f41fb8c0f27598478d6aa1fb46b
Sumit Chavhan <srchavhan29@gmail.com>
2021-11-28 17:56:06 +0530

    simplified

--- Sumit mock/Question11/app.js
+++ Sumit mock/Question11/app.js
@@ -2,30 +2,17 @@ const inPut = document.querySelector("#input");
 const checkBtn = document.querySelector("#checkbutton");
 const outPut = document.querySelector("#output");
 
-checkBtn.disabled = true
+checkBtn.disabled = true;
 
-inPut.addEventListener("input", function () {
-    let value = inPut.value;
-    if (value.length < 10) {
-        checkBtn.disabled = true;
-        inPut.style.border =  " 1rem solid red "
-        outPut.innerText = 'error'
-    } else {
-        checkBtn.disabled = false;
+inPut.addEventListener('input',enableBtn)
+
+function enableBtn(){
+    if(inPut.value.length >= 10){
+            checkBtn.disabled = false
+            outPut.innerText = "Success"
+            inPut.style.border = "1rem solid green"
+    }else{
+        outPut.innerText = "Error"
+        inPut.style.border = "1rem solid red"
     }
-})
-
-
-checkBtn.addEventListener('click',()=>{
-    let value = inPut.value;
-
-    if (value.length < 10) {
-        checkBtn.disabled = true;
-
-    } else {
-        checkBtn.disabled = false;
-        inPut.style.border =  " 1rem solid green "
-        outPut.innerText = 'successful'
-        outPut.style.color =  "green "
-    }
-});
+}
